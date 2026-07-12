f05cea41026adef15be06d5454e02dc4f6b3dfe2
Mrkod-ER <rohitgurjar.khari@gmail.com>
2026-07-12 13:26:56 +0530

    added driver/routes

---
+++ server/src/modules/drivers/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import * as handler from "./handler.js";
+import { requireAuth, requireRole } from "../../middleware/rbac.js";
+
+const router = Router();
+
+router.get("/", requireAuth, handler.list);
+router.get("/:id", requireAuth, handler.getById);
+router.post("/", requireAuth, requireRole("manager"), handler.create);
+router.put("/:id", requireAuth, requireRole("manager"), handler.update);
+router.delete("/:id", requireAuth, requireRole("admin"), handler.remove);
+
+export { router as driverRoutes };
